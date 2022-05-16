cb7caae493c6b8363afdd90211ca1159bab31d48
wayneliu <wliu@allxon.com>
2022-05-16 11:23:22 +0800

    change to service

--- output_package_jetson/9cdfc058-e3f8-4bf4-8621-d2114eade9d9/uninstall_plugIN.sh
+++ output_package_jetson/9cdfc058-e3f8-4bf4-8621-d2114eade9d9/uninstall_plugIN.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+plugin_appguid=e5c7f65b-1cf7-46e6-af56-d418ac43acc2
+plugin_service=plugIN-upterm.service
 
-plugin_appguid=9cdfc058-e3f8-4bf4-8621-d2114eade9d9
+disable_plugin_service() {
+    if [ -f "/etc/systemd/system/${plugin_service}" ]; then
+        echo "Find the service ${plugin_service} from system, disable the service first."
+        systemctl stop ${plugin_service}
+        systemctl disable ${plugin_service}  
+        rm /etc/systemd/system/${plugin_service} 
+    fi
+}
 
-echo "Stop running plugIN-upterm."
-currentShDirectory=$(dirname ${BASH_SOURCE})
-$currentShDirectory/scripts/commands/uninstall.sh
-$currentShDirectory/scripts/stopPlugin.sh
-rm -rf $currentShDirectory
+remove_plugin() {
+    sudo rm -rf /opt/allxon/plugIN/$plugin_appguid
+}
+
+uninstall_plugIN() {
+
+    disable_plugin_service
+
+    remove_plugin
+
+    exit 0
+}
+
+uninstall_plugIN
